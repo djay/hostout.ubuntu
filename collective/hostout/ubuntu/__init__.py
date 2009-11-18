e618ef6575e8ec8595216e75d0ecad654656debc
Dylan Jay <software@pretaweb.com>
2009-11-19 01:50:52 +1100

    sketch of ubuntu bootstrap

--- collective/hostout/ubuntu/__init__.py
+++ collective/hostout/ubuntu/__init__.py
@@ -29,23 +29,8 @@ class Recipe:
 
     def __init__(self, buildout, name, options):
         self.name, self.options, self.buildout = name, options, buildout
-        supervisor = self.options.get('supervisor','supervisor')
-        self.options['supervisor'] = supervisor
-        bin = buildout['buildout']['bin-directory']
-
         self.options['fabfiles'] = fabfile = resource_filename(__name__, 'fabfile.py')
-
-
-        self.options['pre-commands'] = "%s/%sctl shutdown || echo 'Failed to shutdown'"% (bin,supervisor)
-        self.options['post-commands'] = "%s/%sd shutdown"% (bin,supervisor)
-
-        if self.options.get('init.d') is not None:
-            # based on
-            # http://www.webmeisterei.com/friessnegger/2008/06/03/control-production-buildouts-with-supervisor/
-            self.options['post-commands'] += \
-                "cd /etc/init.d && ln -s %s/%sd %s-%sd" % (bin, name, supervisor)
-            self.options['post-commands'] += \
-                "cd /etc/init.d && update-rc.d %s-%sd defaults" % (name, supervisor)
+        self.options['python-version'] = options.get('python-version','2.4.4')
 
 
     def install(self):
--- collective/hostout/ubuntu/fabfile.py
+++ collective/hostout/ubuntu/fabfile.py
@@ -2,32 +2,65 @@ import os
 from os.path import join, basename, dirname
 
 
-
-def initdsupervisor():
-    hostout = get('hostout')
-
-    # based on
-    # http://www.webmeisterei.com/friessnegger/2008/06/03/control-production-buildouts-with-supervisor/
-    bin = "%s/bin" % hostout.getRemoteBuildoutPath()
-    supervisor = hostout.options['supervisor']
-    sudo('sh -c "cd /etc/init.d && ln -s %s/%sd %s-%sd"' % (bin, supervisor, hostout.name, supervisor))
-    sudo('sh -c "cd /etc/init.d && update-rc.d %s-%sd defaults"' % (hostout.name, supervisor))
-
-
-
 def predeploy():
     hostout = get('hostout')
-    bin = "%s/bin" % hostout.getRemoteBuildoutPath()
-    supervisor = hostout.options['supervisor']
-    sudo("%s/%sctl shutdown || echo 'Failed to shutdown'"% (bin,supervisor) )
-    if hostout.options.get('init.d') is not None:
-        initdsupervisor(hostout)
+    try:
+        run('test -f %s/bin/buildout' % hostout.remote_dir)
+    except:
+        #Install and Update Dependencies
+
+        sudo('apt-get update')
+        sudo('apt-get upgrade')
+
+        sudo('apt-get install python2.4 python2.4-dev python-profiler')
+
+        sudo('apt-get install build-essential python2.4 python2.4-dev \
+        python-setuptools python-imaging python-libxml2 ncurses-dev lynx')
+        sudo('apt-get install libjpeg-dev libfreetype6-dev zlib1g-dev')
+
+        #sudo apt-get install apache2
+
+        #to install Python tools 2.4
+        sudo('wget http://peak.telecommunity.com/dist/ez_setup.py')
+        sudo('python2.4 ez_setup.py')
+
+        #to install PIL
+        sudo('easy_install-2.4 --find-links http://download.zope.org/distribution PILwoTK')
+
+        #if its ok you will see something like this:
+        #--------------------------------------------------------------------
+
+        #*** TKINTER support not available
+
+        #--- JPEG support ok
+
+        #--- ZLIB (PNG/ZIP) support ok
+
+        #--- FREETYPE2 support ok
+
+        #--------------------------------------------------------------------
+
+        # Add the plone user:
+
+        owner = hostout.effective_user
+        sudo('useradd -m %s' % owner)
+
+        #Copy authorized keys to plone user:
+        # cp -rp .ssh ~plone/
+        #root@domU-12-31-38-00-35-27:~$ chown -R plone:plone ~plone/.ssh
+
+        sudo('cd /%s && chown %s:%s' % (hostout.remote_dir,owner,owner))
+        #Login as user plone
+        set(fab_user='%s'%owner)
+
+        run('python bootstrap')
+        run('bin/buildout install lxml')
+        run('bin/buildout')
+
+
 
 
 def postdeploy():
     hostout = get('hostout')
-    bin = "%s/bin" % hostout.getRemoteBuildoutPath()
-    supervisor = hostout.options['supervisor']
-    sudo("%s/%sd shutdown"% (bin,supervisor))
 
 
